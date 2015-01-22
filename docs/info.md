81edd9fce1630e9ea764017605e6695e68b0b441
Salvatore <dested@gmail.com>
2015-01-22 13:10:32 -0700

    initial ionic commit

---
+++ docs/info.md
@@ -0,0 +1,12 @@
+notification every day of how many points you gained or lossed int he ranking
+
+
+in app purchases for spin multipliers that last so many days
+
+ladder based, resets once a week
+	tells you how you ranked this hour
+	this day
+	this week
+	this month
+	this year
+	
---
+++ src/www/js/controllers.js
@@ -0,0 +1,56 @@
+angular.module('spinYourPhone.controllers', [])
+
+.controller('DashCtrl', function($scope,$cordovaDeviceOrientation) {
+
+document.addEventListener("deviceready", function () {
+
+    var options = {
+      frequency: 3000,
+      filter: true     // if frequency is set, filter is ignored
+    }
+
+    var watch = $cordovaDeviceOrientation.watchHeading(options).then(
+      null,
+      function(error) {
+        // An error occurred
+      },
+      function(result) {   // updates constantly (depending on frequency value)
+        var magneticHeading = result.magneticHeading;
+        var trueHeading = result.trueHeading;
+        var accuracy = result.headingAccuracy;
+        var timeStamp = result.timestamp;
+
+        $scope.magneticHeading=magneticHeading;
+        $scope.trueHeading =trueHeading ;
+
+      });
+ 
+  }, false);
+
+
+})
+
+.controller('ChatsCtrl', function($scope, Chats) {
+  $scope.chats = Chats.all();
+  $scope.remove = function(chat) {
+    Chats.remove(chat);
+  }
+})
+
+.controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
+  $scope.chat = Chats.get($stateParams.chatId);
+})
+
+.controller('FriendsCtrl', function($scope, Friends) {
+  $scope.friends = Friends.all();
+})
+
+.controller('FriendDetailCtrl', function($scope, $stateParams, Friends) {
+  $scope.friend = Friends.get($stateParams.friendId);
+})
+
+.controller('AccountCtrl', function($scope) {
+  $scope.settings = {
+    enableFriends: true
+  };
+});
